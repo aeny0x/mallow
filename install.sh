8ec795fa25948f0bb1bd49910aa5643fc70e3cc0
aeny0x <luis.andrade@ctec.ufal.br>
2024-12-24 19:57:22 -0300

    fix install script

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 set -xe
 
-mkdir ~/mallow
-cp ./Mallow.jar ~/mallow
-cp -r ./std ~/mallow
-echo "export PATH='~/mallow:$PATH'" >> ~/.bashrc
-echo "export MALLOW_PATH='$HOME/mallow'" >> ~/.bashrc
+mkdir ~/.mallow
+cp ./Mallow.jar ~/.mallow
+cp -r ./std ~/.mallow
+echo "export PATH='~/.mallow:$PATH'" >> ~/.bashrc
+echo "export MALLOW_PATH='$HOME/.mallow'" >> ~/.bashrc
 echo "java -jar $MALLOW_PATH/Mallow.jar \$1" > mallow
 chmod +x mallow
-mv mallow ~/mallow
+mv mallow ~/.mallow
 echo "source ~/.bashrc or restart shell"
